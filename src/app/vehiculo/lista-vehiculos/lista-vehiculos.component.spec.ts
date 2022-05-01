6b760fd72a602c51c9033527632e409e4f37d47f
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2022-05-01 14:48:18 -0500

    Creación de vehiculos con faker

--- src/app/vehiculo/lista-vehiculos/lista-vehiculos.component.spec.ts
+++ src/app/vehiculo/lista-vehiculos/lista-vehiculos.component.spec.ts
@@ -1,19 +1,24 @@
 /* tslint:disable:no-unused-variable */
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+
 import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { ListaVehiculosComponent } from './lista-vehiculos.component';
+import * as fakerLib from '@faker-js/faker';
+import { Vehiculo } from '../vehiculo';
 
 describe('ListaVehiculosComponent', () => {
+  const faker = fakerLib.default;
   let component: ListaVehiculosComponent;
   let fixture: ComponentFixture<ListaVehiculosComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListaVehiculosComponent ]
-    })
-    .compileComponents();
+      declarations: [ListaVehiculosComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,6 +26,40 @@ describe('ListaVehiculosComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  let artworks = [
+    new Vehiculo(
+      faker.datatype.number(),
+      faker.datatype.string(),
+      faker.datatype.string(),
+      faker.datatype.string(),
+      faker.datatype.number(),
+      faker.datatype.number(),
+      faker.datatype.number(),
+      faker.datatype.string()
+    ),
+    new Vehiculo(
+      faker.datatype.number(),
+      faker.datatype.string(),
+      faker.datatype.string(),
+      faker.datatype.string(),
+      faker.datatype.number(),
+      faker.datatype.number(),
+      faker.datatype.number(),
+      faker.datatype.string()
+    ),
+    ,
+    new Vehiculo(
+      faker.datatype.number(),
+      faker.datatype.string(),
+      faker.datatype.string(),
+      faker.datatype.string(),
+      faker.datatype.number(),
+      faker.datatype.number(),
+      faker.datatype.number(),
+      faker.datatype.string()
+    ),
+  ];
 
   it('should create', () => {
     expect(component).toBeTruthy();

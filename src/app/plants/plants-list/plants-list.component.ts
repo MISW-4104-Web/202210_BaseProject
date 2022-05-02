2fd04fe410ea6e2268619e10b36c14e71227eb19
Diego Castro <df.castrop1@uniandes.edu.co>
2022-05-01 21:09:21 -0500

    Basic module setup

---
+++ src/app/plants/plants-list/plants-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plants-list',
+  templateUrl: './plants-list.component.html',
+  styleUrls: ['./plants-list.component.css']
+})
+export class PlantsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/plants/plants.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlantsListComponent } from './plants-list/plants-list.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [PlantsListComponent],
+  declarations: [PlantsListComponent]
+})
+export class PlantsModule { }

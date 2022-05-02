64a76f3881cfbee830075acbd4cdf3d7594b234a
Diego Castro <df.castrop1@uniandes.edu.co>
2022-05-01 21:19:44 -0500

    Defining a Data Class for dealing with Plants components

--- src/app/plants/plants-list/plants-list.component.ts
+++ src/app/plants/plants-list/plants-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Plant } from '../plant';
+
 
 @Component({
   selector: 'app-plants-list',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plants-list.component.css']
 })
 export class PlantsListComponent implements OnInit {
-
+  plants: Array<Plant> = [];
   constructor() { }
 
   ngOnInit() {
